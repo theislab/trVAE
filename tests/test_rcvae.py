7ad5245f42442c974216750ce51ddedc9d5d8164
m0hammadl <mo.lotfollahi@gmail.com>
2019-05-27 15:28:32 +0200

    Add multi-modal support

--- tests/test_rcvae.py
+++ tests/test_rcvae.py
@@ -27,6 +27,10 @@ DATASETS = {
 
     "Salmonella": {"name": 'salmonella', "source_key": "Control", "target_key": 'Salmonella',
                    "cell_type": "cell_label", 'spec_cell_types': []},
+
+    "multimodal": {"name": 'multimodal', "source_key": "RNA-seq", "target_key": 'ATAC-seq',
+                   "cell_type": "modality", 'spec_cell_types': []},
+
 }
 
 
@@ -82,6 +86,48 @@ def train_network(data_dict=None,
 
         print(f"Model for {cell_type} has been trained")
 
+
+
+def train_network_multi(data_dict=None,
+                  z_dim=100,
+                  mmd_dimension=256,
+                  alpha=0.001,
+                  beta=100,
+                  kernel='multi-scale-rbf',
+                  n_epochs=500,
+                  batch_size=512,
+                  dropout_rate=0.2,
+                  learning_rate=0.001,
+                  ):
+    data_name = data_dict['name']
+    target_key = data_dict.get('target_key', None)
+    print(data_name)
+
+    train_data = sc.read(f"../data/{data_name}/train_{data_name}.h5ad")
+    valid_data = sc.read(f"../data/{data_name}/valid_{data_name}.h5ad")
+
+
+
+    network = rcvae.RCVAE(x_dimension=train_data.shape[1],
+                          z_dimension=z_dim,
+                          mmd_dimension=mmd_dimension,
+                          alpha=alpha,
+                          beta=beta,
+                          kernel=kernel,
+                          learning_rate=learning_rate,
+                          train_with_fake_labels=False,
+                          model_path=f"../models/RCVAE/{data_name}/{z_dim}/",
+                          dropout_rate=dropout_rate)
+
+    network.train(train_data,
+                  use_validation=True,
+                  valid_data=valid_data,
+                  n_epochs=n_epochs,
+                  batch_size=batch_size,
+                  verbose=2,
+                  early_stop_limit=600,
+                  shuffle=False,
+                  save=True)
 
 def reconstruct_whole_data(data_dict={}, z_dim=100):
     data_name = data_dict.get('name', None)
@@ -286,6 +332,90 @@ def visualize_trained_network_results(data_dict, z_dim=100):
         plt.close("all")
 
 
+
+
+def visualize_trained_network_results_multimodal(data_dict, z_dim=100):
+    plt.close("all")
+    data_name = data_dict.get('name', None)
+    source_key = data_dict.get('source_key', None)
+    target_key = data_dict.get('target_key', None)
+
+    data = sc.read(f"../data/{data_name}/train_{data_name}.h5ad")
+    path_to_save = f"../results/RCVAE/{data_name}/{z_dim}/{source_key} to {target_key}/Visualizations/"
+    os.makedirs(path_to_save, exist_ok=True)
+    sc.settings.figdir = os.path.abspath(path_to_save)
+
+
+    network = rcvae.RCVAE(x_dimension=data.shape[1],
+                          z_dimension=z_dim,
+                          model_path=f"../models/RCVAE/{data_name}/{z_dim}/", )
+    network.restore_model()
+    if sparse.issparse(data.X):
+        data.X = data.X.A
+
+    feed_data = data.X
+    train_labels, _ = rcvae.label_encoder(data)
+    fake_labels = np.ones(train_labels.shape)
+    latent_with_true_labels = network.to_latent(feed_data, train_labels)
+    latent_with_fake_labels = network.to_latent(feed_data, fake_labels)
+    mmd_latent_with_true_labels = network.to_mmd_layer(network, feed_data, train_labels, feed_fake=False)
+    mmd_latent_with_fake_labels = network.to_mmd_layer(network, feed_data, train_labels, feed_fake=True)
+
+    import matplotlib as mpl
+    mpl.rcParams.update(mpl.rcParamsDefault)
+
+    latent_with_true_labels = sc.AnnData(X=latent_with_true_labels)
+    latent_with_true_labels.obs['condition'] = data.obs['condition'].values
+    # latent_with_true_labels.obs[cell_type_key] = data.obs[cell_type_key].values
+
+    latent_with_fake_labels = sc.AnnData(X=latent_with_fake_labels)
+    latent_with_fake_labels.obs['condition'] = data.obs['condition'].values
+    # latent_with_fake_labels.obs[cell_type_key] = data.obs[cell_type_key].values
+
+    mmd_latent_with_true_labels = sc.AnnData(X=mmd_latent_with_true_labels)
+    mmd_latent_with_true_labels.obs['condition'] = data.obs['condition'].values
+    # mmd_latent_with_true_labels.obs[cell_type_key] = data.obs[cell_type_key].values
+
+    mmd_latent_with_fake_labels = sc.AnnData(X=mmd_latent_with_fake_labels)
+    mmd_latent_with_fake_labels.obs['condition'] = data.obs['condition'].values
+    # mmd_latent_with_fake_labels.obs[cell_type_key] = data.obs[cell_type_key].values
+
+    color = ['condition']
+
+    sc.pp.neighbors(data)
+    sc.tl.umap(data)
+    sc.pl.umap(data, color=color,
+               save=f'_{data_name}_train_data',
+               show=False)
+
+    sc.pp.neighbors(latent_with_true_labels)
+    sc.tl.umap(latent_with_true_labels)
+    sc.pl.umap(latent_with_true_labels, color=color,
+               save=f"_{data_name}_latent_with_true_labels",
+               show=False)
+
+    sc.pp.neighbors(latent_with_fake_labels)
+    sc.tl.umap(latent_with_fake_labels)
+    sc.pl.umap(latent_with_fake_labels, color=color,
+               save=f"_{data_name}__latent_with_fake_labels",
+               show=False)
+
+    sc.pp.neighbors(mmd_latent_with_true_labels)
+    sc.tl.umap(mmd_latent_with_true_labels)
+    sc.pl.umap(mmd_latent_with_true_labels, color=color,
+               save=f"_{data_name}_mmd_latent_with_true_labels",
+               show=False)
+
+    sc.pp.neighbors(mmd_latent_with_fake_labels)
+    sc.tl.umap(mmd_latent_with_fake_labels)
+    sc.pl.umap(mmd_latent_with_fake_labels, color=color,
+               save=f"_{data_name}_mmd_latent_with_fake_labels",
+               show=False)
+    plt.close("all")
+
+
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Sample a trained autoencoder.')
     arguments_group = parser.add_argument_group("Parameters")
@@ -305,7 +435,7 @@ if __name__ == '__main__':
                                  help='Maximum Number of epochs for training')
     arguments_group.add_argument('-c', '--batch_size', type=int, default=512, required=False,
                                  help='Batch Size')
-    arguments_group.add_argument('-r', '--dropout_rate', type=float, default=0.4, required=False,
+    arguments_group.add_argument('-r', '--dropout_rate', type=float, default=0.2, required=False,
                                  help='Dropout ratio')
     arguments_group.add_argument('-l', '--learning_rate', type=float, default=0.001, required=False,
                                  help='Learning rate of optimizer')
@@ -318,7 +448,8 @@ if __name__ == '__main__':
     del args['data']
     if args['do_train'] == 1:
         del args['do_train']
-        train_network(data_dict=data_dict, **args)
-        visualize_trained_network_results(data_dict, z_dim=args['z_dim'])
-    reconstruct_whole_data(data_dict, args['z_dim'])
+        # train_network(data_dict=data_dict, **args)
+        train_network_multi(data_dict=data_dict, **args)
+        visualize_trained_network_results_multimodal(data_dict, z_dim=args['z_dim'])
+    # reconstruct_whole_data(data_dict,s['z_dim'])
     print(f"Model for {data_dict['name']} has been trained and sample results are ready!")
