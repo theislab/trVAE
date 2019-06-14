6a19b6aa1c89f14ac093ebf0f20a8c6cb902a632
Mohsen Naghipourfar <mn7697np@gmail.com>
2019-06-14 11:10:14 +0430

    fix minor bug in test

--- tests/test_rccvae.py
+++ tests/test_rccvae.py
@@ -222,7 +222,7 @@ def evaluate_network(data_dict=None, z_dim=100, n_files=5, k=5, arch_style=1, pr
         if sparse.issparse(valid_data.X):
             valid_data.X = valid_data.X.A
 
-        source_images_train = train_data[valid_data.obs["condition"] == source_key].X
+        source_images_train = train_data[train_data.obs["condition"] == source_key].X
         source_images_valid = valid_data[valid_data.obs["condition"] == source_key].X
 
         source_images_train = np.reshape(source_images_train, (-1, img_width, img_height, n_channels))
