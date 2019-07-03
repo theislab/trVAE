719daa9984702b0b49f846a11fd40aadd24a5c8f
Mohsen Naghipourfar <mn7697np@gmail.com>
2019-07-03 16:31:58 +0200

    implementation trick

--- tests/hyperopt_rcvae_multi.py
+++ tests/hyperopt_rcvae_multi.py
@@ -46,12 +46,12 @@ def data(data_key):
     valid_data = sc.read(f"./data/{data_name}/valid_{data_name}.h5ad")
 
     def inner_data():
-        return train_data, valid_data, data_key
+        return train_data, valid_data
 
     return inner_data
 
 
-def create_model(train_data, valid_data, data_key):
+def preprare_model(data_key):
     DATASETS = {
         "HpolySal": {'name': 'Hpoly+Salmonella', 'need_merge': True,
                      "name1": 'hpoly', 'name2': 'salmonella',
@@ -85,64 +85,67 @@ def create_model(train_data, valid_data, data_key):
     label_encoder = data_dict['label_encoder']
     condition_key = data_dict['condition']
 
-    net_train_data = train_data.copy()[~(train_data.obs[condition_key].isin(target_keys))]
-    net_valid_data = valid_data.copy()[~(valid_data.obs[condition_key].isin(target_keys))]
+    def create_model(train_data, valid_data):
+        net_train_data = train_data.copy()[~(train_data.obs[condition_key].isin(target_keys))]
+        net_valid_data = valid_data.copy()[~(valid_data.obs[condition_key].isin(target_keys))]
 
-    n_conditions = len(net_train_data.obs[condition_key].unique().tolist())
-    arch_style = 2 if data_name == 'cytof' else 1
+        n_conditions = len(net_train_data.obs[condition_key].unique().tolist())
+        arch_style = 2 if data_name == 'cytof' else 1
 
-    network = rcvae.RCVAEMulti(x_dimension=net_train_data.shape[1],
-                               z_dimension={{choice([20, 40, 50, 60, 80, 100, 200])}},
-                               arch_style=arch_style,
-                               n_conditions=n_conditions,
-                               mmd_dimension={{choice([4, 6, 8, 10, 12, 14, 16])}},
-                               alpha={{choice([1.0, 0.1, 0.01, 0.001, 0.0001, 0.00001, 0.000001])}},
-                               beta={{choice([50, 100, 200, 400, 600, 800, 1000])}},
-                               kernel='rbf',
-                               learning_rate={{choice([0.001, 0.0001, 0.00001])}},
-                               model_path=f"../models/RCVAEMulti/{data_name}/hyperopt/",
-                               dropout_rate={{choice([0.1, 0.2, 0.5, 0.75])}})
+        network = rcvae.RCVAEMulti(x_dimension=net_train_data.shape[1],
+                                   z_dimension={{choice([20, 40, 50, 60, 80, 100, 200])}},
+                                   arch_style=arch_style,
+                                   n_conditions=n_conditions,
+                                   mmd_dimension={{choice([4, 6, 8, 10, 12, 14, 16])}},
+                                   alpha={{choice([1.0, 0.1, 0.01, 0.001, 0.0001, 0.00001, 0.000001])}},
+                                   beta={{choice([50, 100, 200, 400, 600, 800, 1000])}},
+                                   kernel='rbf',
+                                   learning_rate={{choice([0.001, 0.0001, 0.00001])}},
+                                   model_path=f"../models/RCVAEMulti/{data_name}/hyperopt/",
+                                   dropout_rate={{choice([0.1, 0.2, 0.5, 0.75])}})
 
-    network.train(net_train_data,
-                  label_encoder,
-                  condition_key,
-                  use_validation=True,
-                  valid_data=net_valid_data,
-                  n_epochs=5000,
-                  batch_size={{choice([32, 64, 128, 256, 512, 1024, 2048])}},
-                  verbose=2,
-                  early_stop_limit=50,
-                  monitor='val_loss',
-                  shuffle=True,
-                  save=False)
+        network.train(net_train_data,
+                      label_encoder,
+                      condition_key,
+                      use_validation=True,
+                      valid_data=net_valid_data,
+                      n_epochs=5000,
+                      batch_size={{choice([32, 64, 128, 256, 512, 1024, 2048])}},
+                      verbose=2,
+                      early_stop_limit=50,
+                      monitor='val_loss',
+                      shuffle=True,
+                      save=False)
 
-    source_condition, target_condition, _, source_label, target_label = data_dict['transition']
-    source_adata = train_data.copy()[train_data.obs[condition_key] == source_condition]
+        source_condition, target_condition, _, source_label, target_label = data_dict['transition']
+        source_adata = train_data.copy()[train_data.obs[condition_key] == source_condition]
 
-    source_labels = np.zeros(source_adata.shape[0]) + source_label
-    target_labels = np.zeros(source_adata.shape[0]) + target_label
+        source_labels = np.zeros(source_adata.shape[0]) + source_label
+        target_labels = np.zeros(source_adata.shape[0]) + target_label
 
-    pred_target = network.predict(source_adata,
-                                  encoder_labels=source_labels,
-                                  decoder_labels=target_labels)
+        pred_target = network.predict(source_adata,
+                                      encoder_labels=source_labels,
+                                      decoder_labels=target_labels)
 
-    pred_adata = anndata.AnnData(X=pred_target)
-    pred_adata.var_names = source_adata.var_names
+        pred_adata = anndata.AnnData(X=pred_target)
+        pred_adata.var_names = source_adata.var_names
 
-    pred_target = pred_adata.copy()
-    real_target = train_data.copy()[train_data.obs[condition_key] == target_condition]
+        pred_target = pred_adata.copy()
+        real_target = train_data.copy()[train_data.obs[condition_key] == target_condition]
 
-    x_var = np.var(pred_target.X, axis=0)
-    y_var = np.var(real_target.X, axis=0)
-    m, b, r_value_var, p_value, std_err = stats.linregress(x_var, y_var)
+        x_var = np.var(pred_target.X, axis=0)
+        y_var = np.var(real_target.X, axis=0)
+        m, b, r_value_var, p_value, std_err = stats.linregress(x_var, y_var)
 
-    x_mean = np.mean(pred_target.X, axis=0)
-    y_mean = np.mean(real_target.X, axis=0)
-    m, b, r_value_mean, p_value, std_err = stats.linregress(x_mean, y_mean)
+        x_mean = np.mean(pred_target.X, axis=0)
+        y_mean = np.mean(real_target.X, axis=0)
+        m, b, r_value_mean, p_value, std_err = stats.linregress(x_mean, y_mean)
 
-    best_reg = r_value_var + r_value_mean
-    print(f'Best Reg of model: ({r_value_mean}, {r_value_var}, {best_reg})')
-    return {'loss': -best_reg, 'status': STATUS_OK, 'model': network.cvae_model}
+        best_reg = r_value_var + r_value_mean
+        print(f'Best Reg of model: ({r_value_mean}, {r_value_var}, {best_reg})')
+        return {'loss': -best_reg, 'status': STATUS_OK, 'model': network.cvae_model}
+
+    return create_model
 
 
 if __name__ == '__main__':
@@ -154,7 +157,7 @@ if __name__ == '__main__':
     args = vars(parser.parse_args())
     data_key = args['data']
 
-    best_run, best_model = optim.minimize(model=create_model,
+    best_run, best_model = optim.minimize(model=preprare_model(data_key),
                                           data=data(data_key),
                                           algo=tpe.suggest,
                                           max_evals=2,
