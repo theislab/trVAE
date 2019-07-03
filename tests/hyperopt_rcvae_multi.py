c4896b482bf824b9b61c464f32b4e3160264fbd3
Mohsen Naghipourfar <mn7697np@gmail.com>
2019-07-03 16:01:18 +0200

    update hyperopt

--- tests/hyperopt_rcvae_multi.py
+++ tests/hyperopt_rcvae_multi.py
@@ -1,5 +1,7 @@
 from __future__ import print_function
 
+import argparse
+
 import anndata
 import numpy as np
 import scanpy as sc
@@ -10,72 +12,116 @@ from scipy import stats
 
 import rcvae
 
+parser = argparse.ArgumentParser(description='Sample a trained autoencoder.')
+arguments_group = parser.add_argument_group("Parameters")
+arguments_group.add_argument('-d', '--data', type=str, required=True,
+                             help='name of dataset you want to train')
+
+args = vars(parser.parse_args())
+data_key = args['data']
+
 
 def data():
-    data_name = 'cytof'
+    global data_key
+    DATASETS = {
+        "HpolySal": {'name': 'Hpoly+Salmonella', 'need_merge': True,
+                     "name1": 'hpoly', 'name2': 'salmonella',
+                     'source_conditions': ['Control', 'Hpoly.Day10'],
+                     'target_conditions': ['Salmonella'],
+                     'transition': ('ctrl_to_hpoly', 'Salmonella', '(ctrl_to_hpoly)_to_sal'),
+                     "cell_type": "cell_label", 'spec_cell_types': ['Stem']},
+
+        "Cytof": {'name': 'cytof', 'need_merge': False,
+                  'source_conditions': ['Basal', 'Bez', 'Das', 'Tof'],
+                  'target_conditions': ['Bez+Das', 'Bez+Tof'],
+                  'transition': ('Basal_to_Bez', 'Bez+Tof', '(Basal_to_Bez)_to_Bez+Tof', 1, 5),
+                  'label_encoder': {'Basal': 0, 'Bez': 1, 'Das': 2, 'Tof': 3, 'Bez+Das': 4, 'Bez+Tof': 5},
+                  'cell_type': 'cell_label'},
+
+        "EndoNorm": {'name': 'endo_norm', 'need_merge': False,
+                     'source_conditions': ['Ctrl', 'GLP1', 'Estrogen', 'PEG-insulin', 'Vehicle-STZ', ],
+                     'target_conditions': ['GLP1-E', 'GLP1-E + PEG-insulin'],
+                     'transition': ('Estrogen', 'GLP1-E', 'Estrogen_to_GLP1-E', 2, 5),
+                     'label_encoder': {'Ctrl': 0, 'GLP1': 1, 'Estrogen': 2, 'PEG-insulin': 3, 'Vehicle-STZ': 4,
+                                       'GLP1-E': 5,
+                                       'GLP1-E + PEG-insulin': 6},
+                     'spec_cell_types': ['beta'],
+                     'condition': 'treatment',
+                     'cell_type': 'groups_named_broad'},
+
+    }
+
+    data_dict = DATASETS[data_key]
+    data_name = data_dict['name']
+
     train_data = sc.read(f"./data/{data_name}/train_{data_name}.h5ad")
     valid_data = sc.read(f"./data/{data_name}/valid_{data_name}.h5ad")
-    return train_data, valid_data
+    return train_data, valid_data, data_dict
 
 
-def create_model(train_data, valid_data):
-    data_name = 'cytof'
-    target_keys = ['Bez+Das', 'Bez+Tof']
-    label_encoder = {'Basal': 0, 'Bez': 1, 'Das': 2, 'Tof': 3, 'Bez+Das': 4, 'Bez+Tof': 5}
+def create_model(train_data, valid_data, data_dict):
+    data_name = data_dict['name']
+    target_keys = data_dict['target_conditions']
+    label_encoder = data_dict['label_encoder']
+    condition_key = data_dict['condition']
 
-    net_train_data = train_data.copy()[~(train_data.obs['condition'].isin(target_keys))]
-    net_valid_data = valid_data.copy()[~(valid_data.obs['condition'].isin(target_keys))]
+    net_train_data = train_data.copy()[~(train_data.obs[condition_key].isin(target_keys))]
+    net_valid_data = valid_data.copy()[~(valid_data.obs[condition_key].isin(target_keys))]
+
+    n_conditions = len(net_train_data.obs[condition_key].unique().tolist())
+    arch_style = 2 if data_name == 'cytof' else 1
 
     network = rcvae.RCVAEMulti(x_dimension=net_train_data.shape[1],
-                               z_dimension={{choice([2, 4, 6, 8, 10, 12])}},
-                               arch_style=2,
-                               n_conditions=4,
+                               z_dimension={{choice([20, 40, 50, 60, 80, 100, 200])}},
+                               arch_style=arch_style,
+                               n_conditions=n_conditions,
                                mmd_dimension={{choice([4, 6, 8, 10, 12, 14, 16])}},
                                alpha={{choice([1.0, 0.1, 0.01, 0.001, 0.0001, 0.00001, 0.000001])}},
                                beta={{choice([50, 100, 200, 400, 600, 800, 1000])}},
                                kernel='rbf',
-                               learning_rate={{choice([0.01, 0.001, 0.0001, 0.00001])}},
+                               learning_rate={{choice([0.001, 0.0001, 0.00001])}},
                                model_path=f"../models/RCVAEMulti/{data_name}/hyperopt/",
                                dropout_rate={{choice([0.1, 0.2, 0.5, 0.75])}})
 
     network.train(net_train_data,
                   label_encoder,
+                  condition_key,
                   use_validation=True,
                   valid_data=net_valid_data,
                   n_epochs=5000,
-                  batch_size={{choice([16, 32, 64, 128, 256, 512, 1024, 2048])}},
+                  batch_size={{choice([32, 64, 128, 256, 512, 1024, 2048])}},
                   verbose=2,
                   early_stop_limit=50,
                   monitor='val_loss',
                   shuffle=True,
                   save=False)
 
-    source_adata = train_data.copy()[train_data.obs['condition'] == 'Bez']
+    source_condition, target_condition, _, source_label, target_label = data_dict['transition']
+    source_adata = train_data.copy()[train_data.obs[condition_key] == source_condition]
 
-    source_labels = np.zeros(source_adata.shape[0]) + 1
-    target_labels = np.zeros(source_adata.shape[0]) + 3
+    source_labels = np.zeros(source_adata.shape[0]) + source_label
+    target_labels = np.zeros(source_adata.shape[0]) + target_label
 
     pred_target = network.predict(source_adata,
                                   encoder_labels=source_labels,
                                   decoder_labels=target_labels)
 
     pred_adata = anndata.AnnData(X=pred_target)
-    pred_adata.obs['condition'] = ['Bez_to_Bez+Das'] * pred_target.shape[0]
     pred_adata.var_names = source_adata.var_names
 
-    ctrl = pred_adata
-    stim = train_data.copy()[train_data.obs['condition'] == 'Bez+Das']
+    pred_target = pred_adata.copy()
+    real_target = train_data.copy()[train_data.obs[condition_key] == target_condition]
 
-    x_var = np.var(ctrl.X, axis=0)
-    y_var = np.var(stim.X, axis=0)
+    x_var = np.var(pred_target.X, axis=0)
+    y_var = np.var(real_target.X, axis=0)
     m, b, r_value_var, p_value, std_err = stats.linregress(x_var, y_var)
 
-    x_mean = np.mean(ctrl.X, axis=0)
-    y_mean = np.mean(stim.X, axis=0)
+    x_mean = np.mean(pred_target.X, axis=0)
+    y_mean = np.mean(real_target.X, axis=0)
     m, b, r_value_mean, p_value, std_err = stats.linregress(x_mean, y_mean)
 
-    best_reg = 1.5 * r_value_var + r_value_mean
-    print('Best Reg Var of model:', best_reg)
+    best_reg = r_value_var + r_value_mean
+    print(f'Best Reg of model: ({r_value_mean}, {r_value_var}, {best_reg})')
     return {'loss': -best_reg, 'status': STATUS_OK, 'model': network.cvae_model}
 
 
