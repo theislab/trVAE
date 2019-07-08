909653f9848edc4994ab5851c7994e55fc44f204
Mohsen Naghipourfar <mn7697np@gmail.com>
2019-07-08 17:08:18 +0200

    Add RCVAEATAC

--- rcvae/models/__init__.py
+++ rcvae/models/__init__.py
@@ -4,5 +4,6 @@ from ._rae import RAE
 from ._rccvae import RCCVAE
 from ._rcvae import RCVAE
 from ._rcvae_multi import RCVAEMulti
+from ._rcvae_atac import RCVAEATAC
 from ._vae import VAE
 from .utils import label_encoder
