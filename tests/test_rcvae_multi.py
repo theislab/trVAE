f19cd069b5c0b7304e08490393caac1680609b93
Mohsen Naghipourfar <mn7697np@gmail.com>
2019-07-10 17:37:56 +0200

    change dtype

--- tests/test_rcvae_multi.py
+++ tests/test_rcvae_multi.py
@@ -499,8 +499,9 @@ def visualize_batch_correction(data_dict, z_dim=100, mmd_dimension=128, arch_sty
 
         mmd_latent_with_true_labels.obs['mmd'] = 'others'
         mmd_latent_with_true_labels.obs['mmd'] = mmd_latent_with_true_labels.obs['mmd'].astype('category')
-        mmd_latent_with_true_labels.obs['mmd'].cat.add_categories([f'alpha-{target_keys[0]}', 'alpha-others'],
-                                                                  inplace=True)
+        mmd_latent_with_true_labels.obs['mmd'].cat.add_categories([f'alpha-{target_keys[0]}'], inplace=True)
+        mmd_latent_with_true_labels.obs['mmd'].cat.add_categories([f'alpha-others'], inplace=True)
+        print(mmd_latent_with_true_labels.obs['mmd'].cat.categories)
         mmd_latent_with_true_labels.obs.loc[((mmd_latent_with_true_labels.obs[condition_key] == target_keys[0]) &
                                              mmd_latent_with_true_labels.obs[
                                                  cell_type_key] == cell_type), 'mmd'] = f'alpha-{target_keys[0]}'
