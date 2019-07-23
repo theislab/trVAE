ef9cb4548841c82605c139a1b0ce86e25e82a3a0
Mohsen Naghipourfar <mn7697np@gmail.com>
2019-07-23 18:07:17 +0200

    add option to normalization

--- rcvae/utils.py
+++ rcvae/utils.py
@@ -2,15 +2,12 @@ import numpy as np
 import scanpy as sc
 
 
-def normalize(adata, filter_min_counts=True, size_factors=True, normalize_input=True, logtrans_input=True):
+def normalize(adata, filter_min_counts=True, size_factors=True, normalize_input=True, logtrans_input=True, n_top_genes=2000):
     if filter_min_counts:
         sc.pp.filter_genes(adata, min_counts=1)
         sc.pp.filter_cells(adata, min_counts=1)
 
-    if size_factors or normalize_input or logtrans_input:
-        adata.raw = adata.copy()
-    else:
-        adata.raw = adata
+    adata_count = adata.copy()
 
     if size_factors:
         sc.pp.normalize_per_cell(adata)
@@ -21,8 +18,19 @@ def normalize(adata, filter_min_counts=True, size_factors=True, normalize_input=
     if logtrans_input:
         sc.pp.log1p(adata)
 
+    if n_top_genes > 0 and adata.shape[1] > n_top_genes:
+        sc.pp.highly_variable_genes(adata, n_top_genes=n_top_genes)
+        genes = adata.var['highly_variable']
+        adata = adata[:, genes]
+        adata_count = adata_count[:, genes]
+
     if normalize_input:
         sc.pp.scale(adata)
+
+    if size_factors or normalize_input or logtrans_input:
+        adata.raw = adata_count.copy()
+    else:
+        adata.raw = adata_count
 
     return adata
 
