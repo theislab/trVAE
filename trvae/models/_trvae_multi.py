de4f0302914b4e1928b008c33e422ed523315daa
M0hammadL <33202701+M0hammadL@users.noreply.github.com>
2019-09-13 11:23:40 +0200

    Rename mmd-mode variable

--- trvae/models/_trvae_multi.py
+++ trvae/models/_trvae_multi.py
@@ -82,7 +82,7 @@ class trVAEMulti:
         self.model_to_use = kwargs.get("model_path", "./")
         self.kernel_method = kwargs.get("kernel", "multi-scale-rbf")
         self.output_activation = kwargs.get("output_activation", 'relu')
-        self.mmd_computation_way = kwargs.get("mmd_computation_way", "general")
+        self.mmd_calc_mode = kwargs.get("mmd_computation_way", "general")
         self.clip_value = kwargs.get('clip_value', 1e6)
         self.lambda_l1 = kwargs.get('lambda_l1', 0.0)
         self.lambda_l2 = kwargs.get('lambda_l2', 0.0)
